9996f9813cee0885d7b13e4bcd682f79ab0ee133
Mukund-Vyas <mukund39ppsv@gmail.com>
2024-09-04 00:57:54 +0530

    update index page image with resize and webp, use Next Image instead of img

--- exotica/next.config.mjs
+++ exotica/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'exoticalingerie.in'],
+    domains: ['localhost', 'www.exoticalingerie.in', 'www.api.exoticalingerie.in'],
   },
 };
 
